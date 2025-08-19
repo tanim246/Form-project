43c2d60892908e6c804479ba789cf15963dd0285
Tanim <mdtasnimhossaintanim@gmail.com>
2025-08-19 23:02:19 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,120 @@
-// rafce
+// import React from 'react'
 
 import { useState } from "react";
+import { IoEyeSharp } from "react-icons/io5";
 
 const App = () => {
-  //const [count, setstate] = useState(initialValue);
-  const [count, setCount] = useState(0);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const dataCaptureEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const dataCapturePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  console.log(email,password
 
-  function pluse() {
-    setCount(count + 1);
-  }
-  function minus() {
-    if (count > 0) {
-      setCount(count - 1);
-    } else {
-      setCount(0);
-    };
-  }
-  function reset() {
-    setCount(0);
+  )
+  console.log(email, password);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const clickEyes = () => {
+    setShowPassword(!showPassword);
   };
 
   return (
-    <div className="flex justify-center items-center  h-[100vh]">
-      <div className="flex flex-col">
-        <div className="flex flex-col items-center space-y-3">
-          <h1 className="text-4xl mb-12  font-semibold">Counter Application</h1>
-          <span className="w-[100px] h-[100px] mb-8 flex justify-center items-center rounded-full text-4xl border border-4 border-yellow-400 ">
-            {count}
-          </span>
+    <div>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="Your Company"
+            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
+            Sign in to your account
+          </h2>
         </div>
-        <div className="flex flex-col space-y-4 items-center">
-          <div className="space-x-2">
-            {/* pluse button */}
-            <button
-              className="bg-sky-600 cursor-pointer  text-white rounded-full font-semibold px-10 py-2 shadow-gray-500"
-              onClick={pluse}
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-100"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  onChange={dataCaptureEmail}
+                  value={email}
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-gray-100"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-400 hover:text-indigo-300"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2 relative ">
+                <input
+                  value={password}
+                  onChange={dataCapturePassword}
+                  id="password"
+                  name="password"
+                  type={showPassword ? "text" : "password"}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+                <IoEyeSharp
+                  onClick={clickEyes}
+                  className="absolute top-2.5 right-4 text-gray-400 cursor-pointer "
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-gray-400">
+            Not a member?{" "}
+            <a
+              href="#"
+              className="font-semibold text-indigo-400 hover:text-indigo-300"
             >
-              <i class="fa-solid fa-plus"></i>
-            </button>
-            <button
-              className="bg-green-600 cursor-pointer  font-semibold text-white rounded-full px-10 py-2 shadow-gray-500 "
-              onClick={minus}
-            >
-              <i class="fa-solid fa-minus"></i>
-            </button>
-          </div>
-          <div>
-            <button
-              className="bg-rose-600 cursor-pointer   font-semibold text-white rounded-full px-10 py-2 shadow-gray-500 "
-              onClick={reset}
-            >
-              Reset
-            </button>
-          </div>
+              Start a 14 day free trial
+            </a>
+          </p>
         </div>
-        <p className="text-center mt-8 text-[14px]">
-          Made By:{" "}
-          <a className="text-blue-900 font-semibold">Hussen Md. Tanim</a>
-        </p>
       </div>
     </div>
   );
